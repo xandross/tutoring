5a2e777e76d92fcd14d6b7e49574a5c12b5eca09
Imre Varga <vargaimretamas@gmail.com>
2022-06-08 19:52:19 +0200

    list comp example

---
+++ list_comprehension.py
@@ -0,0 +1,51 @@
+import datetime
+
+class Person:
+    def __init__(self, id: int, first_name: str, last_name: str, birth_date: datetime.date):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+
+    def __repr__(self):
+        return "{0},{1},{2},{3}".format(self.id, self.first_name, self.last_name, self.birth_date)
+
+
+people = [Person(1,"Janos","Kovacs",datetime.date(1980,1,1)),
+        Person(2,"Gabor","Varga",datetime.date(1981,3,3)),
+        Person(3,"Mariann","Varga",datetime.date(2002,5,10)),
+        Person(4,"Bela","Kovacs",datetime.date(1981,2,3)),
+        Person(5,"Dora","Nagy",datetime.date(1985,5,10)),
+        Person(6,"Zsuzsa","Kiss",datetime.date(1990,8,11)),
+        Person(7,"Geza","Kiss",datetime.date(1990,6,10))]
+
+print("1.) Mindenki minden adata")
+people1 = [
+    p                           # SELECT *
+    for p in people             # FROM PERSON
+]
+print(*people1, sep='\n')
+
+print("2.) K-val kezdődő vezetéknevű személyek nagybetűs keresztnevei")
+people2 = [
+    p.first_name.upper()        # SELECT UPPER(first_name) 
+    for p in people             # FROM PERSON
+    if p.last_name[0] == 'K'    # WHERE last_name like 'K%';
+]
+print(*people2, sep='\n')
+
+print("3.) Kiss vezetéknevűek minden adata")
+
+print("4.) Kiss Zsuzsa id-ja")
+
+print("5.) 1990-01-01 után születettek vezeték és keresztneve space-szel elválasztva")
+
+print("6.) Az össze embernek megfelelő 1-1 új person object, amik id-ja 10-zel nagyobb az eredetinél")
+
+print("7.) egyedi vezetéknevek") # set comprehension
+
+print("8.) {id -> (vezeték + keresztnév)} párok dictionary-ben") # dict comprehension (list input)
+
+print("9.) a 8-as eredményéből a páros id-júak value-ja") # dict comprehension (dict input)
+
+print("10.) Az összes keresztnév összes karaketere egymás után egy listában") # 2 for
